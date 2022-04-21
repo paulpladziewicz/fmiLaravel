2445cb94e30a76b6af074407bdc98f4655a2eb75
paulpladziewicz <ppladziewicz@gmail.com>
2022-04-21 13:16:08 -0400

    included ide helper

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -5,6 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Business
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\BusinessFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Business newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Business newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Business query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Business whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Business whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Business whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Business extends Model
 {
     use HasFactory;
--- app/Models/Person.php
+++ app/Models/Person.php
@@ -5,6 +5,36 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Person
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $published
+ * @property string|null $s3_url
+ * @property string $first_name
+ * @property string $last_name
+ * @property string $about
+ * @property string $interests
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User|null $user
+ * @method static \Database\Factories\PersonFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Person newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Person query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereAbout($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereInterests($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person wherePublished($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereS3Url($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Person whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Person extends Model
 {
     use HasFactory;
